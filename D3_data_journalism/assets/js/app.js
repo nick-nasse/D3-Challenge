fcf8d0880cc3c1c619ab9feb58282f3325e6ec60
Nick Nasse <nasse@Nasses-Pro-Book.local>
2020-10-07 12:09:15 -0700

    js updated with chart area

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,8 +1,27 @@
+// create the chart area
+var svgwidth = 900;
+var svgheight = 600;
 
+var margin = {
+    top: 75,
+    right: 75,
+    bottom: 75, 
+    left: 75
+  };
+
+var width = svgwidth - margin.left - margin.right;
+var height = svgheight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter").append("svg")
+  .attr("width", svgwidth)
+  .attr("height", svgheight);
+
+var chartgroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// load csv
 d3.csv("/assets/data/data.csv").then(function(hdata) {
-    
-    // Step 4: Format the data using the unary + operator
-    hdata.forEach(function(data) {
+        hdata.forEach(function(data) {
         data.healthcare = +data.healthcare;
         data.obesity = +data.obesity;
     });
