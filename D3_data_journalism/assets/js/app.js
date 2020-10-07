a4fb64f333b12390517d1287027aa6c491f7fe06
Nick Nasse <nasse@Nasses-Pro-Book.local>
2020-10-07 11:49:19 -0700

    augmented html and csv load into js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,10 @@
-// @TODO: YOUR CODE HERE!
+
+d3.csv("/assets/data/data.csv").then(function(hdata) {
+    
+    // Step 4: Format the data using the unary + operator
+    hdata.forEach(function(data) {
+        data.healthcare = +data.healthcare;
+        data.obesity = +data.obesity;
+    });
+    console.log(hdata)
+  });
